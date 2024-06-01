5716ef9e134f2dcbb276ad68769f54e63b0ea06b
Michael.S <michael.slyusar@gmail.com>
2024-06-01 11:49:03 +0400

    Base code

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import sqlite3
 from telebot import types
+import random
 
 load_dotenv()
 
@@ -24,7 +25,7 @@ def start(message):
     bot.send_photo(message.chat.id,file)
 
     text = 'Добро пожаловать, странник!✨ На пороге волшебного мира вас встречает мудрый маг. Под светом луны и сиянием звезд он проведет вас через тайны и откровения.🌙Приготовьтесь к удивительному путешествию, где каждое предсказание откроет новые грани вашей души и сердца. Позвольте магу стать вашим проводником в мир предсказаний и мудрости'
-    markup = types.ReplyKeyboardMarkup()
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     markup.row(types.KeyboardButton('Продолжать'))
     
     bot.send_message(message.chat.id,text,reply_markup=markup)
@@ -32,8 +33,26 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_message(callback):
-    if callback.data == 'tarot':
-        bot.send_message(callback.message.chat.id,'Tarot')
+    if callback.data == 'Tarot':
+        text = '"Выбор сделан... Карта дня откроет вам завесу будущего и покажет, какие энергии и события ждут вас сегодня. Позвольте магу раскрыть перед вами тайны этого дня и получить важное предсказание."'
+        print("tarot")
+        random_card = random.randrange(1, 13, 1)
+
+        bot.send_message(callback.message.chat.id, text)
+
+        file = open('./img/'+str(random_card)+'.jpg','rb')
+        bot.send_photo(callback.message.chat.id,file)
+
+        bot.send_message(callback.message.chat.id, 'Tarot prediction')
+
+    elif callback.data == 'Horoscope':
+        text = '"Выбор сделан... Звезды готовы рассказать вам о том, что они приготовили для вас. Прогноз по гороскопу откроет вам небесные секреты и поможет лучше понять, как использовать их в свою пользу."'
+        print("Horoscope")
+        bot.send_message(callback.message.chat.id,text)
+    elif callback.data == 'Affirmation':
+        text = '"Выбор сделан... Мудрый маг приготовил для вас аффирмацию, которая наполнит ваш день позитивной энергией и вдохновением. Повторяйте её, чтобы укрепить свой дух и достичь гармонии."'
+        print("Affirmation")
+        bot.send_message(callback.message.chat.id,text)
 
 
 
@@ -43,10 +62,14 @@ def main_menu(message):
 
     markup = types.InlineKeyboardMarkup()
     btn_tarot = types.InlineKeyboardButton('Карта дня', callback_data='Tarot')
+    btn_horoscope= types.InlineKeyboardButton('Прогноз по гороскопу',callback_data='Horoscope')
+    btn_affirmation= types.InlineKeyboardButton('Аффирмация дня',callback_data='Affirmation')
 
-    markup.add(btn_tarot)
+    markup.row(btn_tarot)
+    markup.row(btn_horoscope,btn_affirmation)
 
-    bot.send_message(message.chat.id, '', reply_markup=markup)
+    text = 'Text for main menu'
+    bot.send_message(message.chat.id, text, reply_markup=markup)
 
 
 bot.polling(none_stop=True)
